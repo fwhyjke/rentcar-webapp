e14300e99f2e6c7991e091d07aa6d8719cc6319b
Ilya Timofeev <timofeevila423@gmail.com>
2023-07-13 05:06:45 +0700

    DRF verify email added

--- users_app/views.py
+++ users_app/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from users_app.models import VerifyEmailToken
 
 
+# I think I don't need a serializer since I only change one field
 class VerifyEmail(APIView):
     def get(self, request, **kwargs):
         token = self.kwargs['token']
