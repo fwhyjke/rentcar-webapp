3403118c161c11e6d3c9e24ff70cabcca9bc0573
Ilya Timofeev <timofeevila423@gmail.com>
2023-07-15 04:35:44 +0700

    Added paths for media-files

--- rentgame/urls.py
+++ rentgame/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rentgame_app.urls')),
     path('account/', include('users_app.urls')),
     re_path('', include('social_django.urls', namespace='social')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
